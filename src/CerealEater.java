8a8be8aea3db313fd6c99374152960fd336cb325
Oalecj <Oalecj@gmail.com>
2014-08-28 15:32:19 -0700

    Bugfixes
    + Custom File Names
    - Possible Bugs
    / Code Cleanup

--- src/CerealEater.java
+++ src/CerealEater.java
@@ -7,28 +7,23 @@ import javax.swing.JOptionPane;
 
 public class CerealEater {
 	public static void main(String[] args) {
-		int dialogButton = JOptionPane.YES_NO_OPTION;
-		int dialogResult = JOptionPane.showConfirmDialog(null, "Load Cereal Types in File?",
-				"CerealEater", dialogButton);
-		if (dialogResult == 0) {
-			try (FileInputStream fs = new FileInputStream("cereal.bin")) {
+		//What file should I load from?
+		String file = (JOptionPane.showInputDialog("What file should I load from? (Don't type .bin)") + ".bin");
+			try (FileInputStream fs = new FileInputStream(file)) {
 				ObjectInputStream oi = new ObjectInputStream(fs);
 				Cereal[] cereal1 = (Cereal[])oi.readObject();
 				//System.out.println(cereal1 + " and  " + cereal2);
 				JOptionPane.showMessageDialog(null, "User likes " + cereal1[0] + " and  " + cereal1[1] + "\nbut does not like " + cereal1[2], "Cereal Types Loaded", 1);
 				oi.close();
 			} catch (FileNotFoundException e) {
-				JOptionPane.showMessageDialog(null, "File not found: " + "cereal.bin", "Fatal error!", 2);
+				JOptionPane.showMessageDialog(null, "File not found: " + file, "Fatal error!", 2);
 				e.printStackTrace();
 			} catch (IOException e) {
-				JOptionPane.showMessageDialog(null, "File IO error: " + "cereal.bin", "Fatal error!", 2);
+				JOptionPane.showMessageDialog(null, "File IO error: " + file, "Fatal error!", 2);
 				e.printStackTrace();
 			} catch (ClassNotFoundException e) {
 				JOptionPane.showMessageDialog(null, "ClassNotFound Error", "Fatal error!", 2);
 				e.printStackTrace();
 			}
-		} else {
-			System.exit(0);
-		}
 	}
 }
--- src/CerealMaker.java
+++ src/CerealMaker.java
@@ -7,23 +7,40 @@ import javax.swing.JOptionPane;
 
 class CerealMaker {
 	public static void main(String[] args) {
-		String name1 = JOptionPane.showInputDialog("Enter the name of a cereal you like!");
-		String name2 = JOptionPane.showInputDialog("Enter the name of another cereal you like!");
-		String name3 = JOptionPane.showInputDialog("Enter the name of another cereal you don't like!");
-		String file = "cereal.bin";
-		Cereal[] cereals = {new Cereal(name1, true), new Cereal(name2, true), new Cereal(name3, true)};
-		try(FileOutputStream netflix = new FileOutputStream(file)) {
-			ObjectOutputStream os = new ObjectOutputStream(netflix);
+		// Get variables to write to file!
+		String name1 = JOptionPane
+				.showInputDialog("Enter the name of a cereal you like!");
+		String name2 = JOptionPane
+				.showInputDialog("Enter the name of another cereal you like!");
+		String name3 = JOptionPane
+				.showInputDialog("Enter the name of another cereal you don't like!");
+		// What file to write to?
+		String file = (JOptionPane
+				.showInputDialog("Name of file to write info? (Don't type .bin)") + ".bin");
+		// Initialize array!
+		Cereal[] cereals = { new Cereal(name1, true), new Cereal(name2, true),
+				new Cereal(name3, true) };
+		// Try ---> Catch
+		try (FileOutputStream neti = new FileOutputStream(file)) {
+			ObjectOutputStream os = new ObjectOutputStream(neti);
+			// Write the array!
 			os.writeObject(cereals);
+			// Close ObjectOutputStream!
 			os.close();
-		JOptionPane.showMessageDialog(null, "Saved output to cereal.bin", "SUCCESS!", 1);
+			// Tell the user the process was a success!
+			JOptionPane.showMessageDialog(null, "Saved output to " + file,
+					"SUCCESS!", 1);
 		} catch (FileNotFoundException e) {
-			JOptionPane.showMessageDialog(null, "File not found: " + file, "Fatal error!", 2);
+			//File can't be created!
+			JOptionPane.showMessageDialog(null, "File not found: " + file,
+					"Fatal error!", 2);
 			e.printStackTrace();
 		} catch (IOException e) {
-			JOptionPane.showMessageDialog(null, "File IO error: " + file, "Fatal error!", 2);
+			//Possible permissions fail?
+			JOptionPane.showMessageDialog(null, "File IO error: " + file,
+					"Fatal error!", 2);
 			e.printStackTrace();
 		}
-		
+
 	}
 }
